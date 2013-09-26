6b6ac8c8a1a417331e80caac6a3f150b385a0bc4
FearFree <fearfree@cubetown.net>
2013-09-26 17:40:08 -0500

    Yum RC1 and format fixes

--- MCMA-Install.sh
+++ MCMA-Install.sh
@@ -1,105 +1,99 @@
 #!/bin/bash
 if [ $USER != "root" ]
 then
-  echo "You must run this script as root."
-  exit 1
+	echo "You must run this script as root."
+	exit 1
 fi
 arch=`uname -m`
 if command -v yum update >/dev/null
 then
-  system="yum"
-  echo "Yum Package Manager Detected"
-  sleep 1
-  if [ "$arch" = "x86_64" ]
-  then
-    echo "64 Bit Detected"
-    arch="64"
-    sleep 1
-  else
-    echo "32 Bit Detected"
-    arch="32"
-    sleep 1
-  fi
+	system="yum"
+	echo "Yum Package Manager Detected"
+	sleep 1
+	if [ "$arch" = "x86_64" ]
+	then
+		echo "64 Bit Detected"
+		arch="64"
+		sleep 1
+	else
+		echo "32 Bit Detected"
+		arch="32"
+		sleep 1
+	fi
 else
-  if command -v apt-get update >/dev/null
-  then
-    system="apt"
-    echo "Apt-Get Package Manager Detected"
-    sleep 1
-    if [ "$arch" = "x86_64" ]
-    then
-      echo "64 Bit Detected"
-      arch="64"
-      sleep 1
-    else
-      echo "32 Bit Detected"
-      arch="32"
-      sleep 1
-    fi
-  else
-    echo "Unable to determins package manager, exiting"
-    system="Unknown"
-    exit 1
-  fi
+	if command -v apt-get update >/dev/null
+	then
+		system="apt"
+		echo "Apt-Get Package Manager Detected"
+		sleep 1
+		if [ "$arch" = "x86_64" ]
+		then
+			echo "64 Bit Detected"
+			arch="64"
+			sleep 1
+		else
+			echo "32 Bit Detected"
+			arch="32"
+			sleep 1
+		fi
+	else
+		echo "Unable to determins package manager, exiting"
+		system="Unknown"
+		exit 1
+	fi
 fi
 
 if [ "$system" = "yum" ]
-  then
-    if [ "$arch" = "64" ]
-    then
-      echo "Initializing 64 Bit RHEL Based System Installation..."
-	  read -p "Non-root user to run McMyAdmin as: " mcuser
-	  read -p "Please enter password for this user (leave blank if user already exists): " mcpass
-	  read -p "Please enter the password you would like for McMyAdmin's admin user: " mcmapass
-	  #read -p "How much RAM would you like to allocate to the Minecraft server, in MB (1024MB per GB): " ram
-	  #Add more variables as needed for MCMA config
-      yum update >/dev/null 2>&1
-	  yum -y -q install java-1.7.0-openjdk.x86_64
-      yum -y -q install screen >/dev/null 2>&1
-      yum -y -q install unzip >/dev/null 2>&1
-
-	  cd /usr/local
-      wget http://mcmyadmin.com/Downloads/etc.zip
-      unzip -qq etc.zip
-	  rm -f etc.zip
-
-	  ret=false
-	  getent passwd $mcuser >/dev/null 2>&1 && ret=true
-	  if $ret; then
-	    echo "The non-root user you specified already exists, continuing..."
-	  else
-	    echo "The non-root user you specified does not yet exist, creating..."
-		adduser $mcuser
-		echo -e "$mcpass\n$mcpass" | (passwd --stdin $mcuser)
-	  fi
-	  #Do the following with sudo -u to non-root user
-	  cd /home/$mcuser
-      sudo -u $mcuser wget -q http://mcmyadmin.com/Downloads/MCMA2_glibc25.zip
-      sudo -u $mcuser unzip -qq MCMA-glibc25.zip >/dev/null 2>&1
-      rm -f MCMA2-glibc.zip
-	 
- 	  sudo -u $mcuser cat > /home/$mcuser/start.sh << EOF
+then
+	if [ "$arch" = "64" ]
+	then
+		echo "Initializing 64 Bit RHEL Based System Installation..."
+		read -p "Non-root user to run McMyAdmin as: " mcuser
+		read -p "Please enter password for this user (leave blank if user already exists): " mcpass
+		read -p "Please enter the password you would like for McMyAdmin's admin user: " mcmapass
+		echo "Starting installation, this make take a few minutes..."
+		#read -p "How much RAM would you like to allocate to the Minecraft server, in MB (1024MB per GB): " ram
+		#Add more variables as needed for MCMA config
+		yum -y update >/dev/null 2>&1
+		yum -y -q install java-1.7.0-openjdk.x86_64 >/dev/null 2>&1
+		yum -y -q install screen >/dev/null 2>&1
+		yum -y -q install unzip >/dev/null 2>&1
+		cd /usr/local
+		wget -q http://mcmyadmin.com/Downloads/etc.zip >/dev/null 2>&1
+		unzip -qq -o etc.zip >/dev/null 2>&1
+		rm -f etc.zip
+		ret=false
+		getent passwd $mcuser >/dev/null 2>&1 && ret=true
+		if $ret; then
+			echo "The non-root user you specified already exists, continuing..."
+		else
+			echo "The non-root user you specified does not yet exist, creating..."
+			adduser $mcuser
+			echo -e "$mcpass\n$mcpass" | (passwd --stdin $mcuser)
+		fi
+		cd /home/$mcuser
+		sudo -u $mcuser wget -q http://mcmyadmin.com/Downloads/MCMA2_glibc25.zip >/dev/null 2>&1
+		sudo -u $mcuser unzip -qq -o MCMA2_glibc25.zip >/dev/null 2>&1
+		rm -f MCMA2_glibc25.zip
+		sudo -u $mcuser cat > /home/$mcuser/start.sh << EOF
 #!/bin/bash
 
 cd /home/$mcuser
 screen -dmS MCMA ./MCMA2_Linux_x86_64
 EOF
-     
-	  echo "Setting Up McMyAdmin Auto-Start..."
-	  cron="@reboot sh /home/$mcuser/start.sh"
-	  sudo -u $mcuser bash <<EOF
+		echo "Setting Up McMyAdmin Auto-Start..."
+		cron="@reboot sh /home/$mcuser/start.sh\n"
+		sudo -u $mcuser bash <<EOF
 (crontab -l; echo "$cron" ) | crontab -
+./MCMA2_Linux_x86_64 -nonotice -setpass $mcmapass -configonly >/dev/null 2>&1
 EOF
-
-	  sudo -u $mcuser ./MCMA2_Linux_x86_64 -nonotice -setpass $mcmapass -configonly
-	  
-	  echo "Installation to this point complete.  Test and check for errors"
- #Finish this....
-    else
-      echo "32 bit operating systems not supported, exiting"
-      #Insert 32 bit yum code (future)
-      exit 1
-    fi
+		echo "Installation to this point complete.  Test and check for errors"
+		#Finish this....
+	else
+		echo "32 bit operating systems not supported, exiting"
+		#Insert 32 bit yum code (future)
+		exit 1
+	fi
 #################################################################
 #File has not been modified from legacy version below this point#
 #################################################################
